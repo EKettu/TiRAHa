911aaf80dceb7da94146e07b21e7ac0f3ad3c245
Elina K <elina.kettunen@helsinki.fi>
2016-09-17 17:15:48 +0300

    toinen viikko alku

--- AstarEtc/src/Main.java
+++ AstarEtc/src/Main.java
@@ -5,9 +5,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //testi
-        //toimiiko...
+        Node startNode = new Node(0,0,0,0,1);
+        Node endNode = new Node(2,2,0,0,2);
+        Astar astar = new Astar();
+        astar.astar(5, startNode,endNode);
     }
-    
 }
