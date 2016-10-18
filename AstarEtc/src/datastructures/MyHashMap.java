9b0a2a7de7151bfc969b6c5756877ea22a69e30a
Elina K <elina.kettunen@helsinki.fi>
2016-10-18 13:02:48 +0300

    .jar-tiedosto

--- AstarEtc/src/datastructures/MyHashMap.java
+++ AstarEtc/src/datastructures/MyHashMap.java
@@ -1,7 +1,7 @@
-
 package datastructures;
 
-public class MyHashMap <K, V> {
+public class MyHashMap<K, V> {
+
     /**
      * Array for entries
      */
@@ -10,16 +10,17 @@ public class MyHashMap <K, V> {
      * Initial capacity of MyHashMap
      */
     private int capacity = 4;
-    
+
     /**
      * An implementation of a custom HashMap
      */
     public MyHashMap() {
         table = new Entry[capacity];
     }
-    
+
     /**
      * Method to add an item to MyHashMap
+     *
      * @param key, a key to which a value is attached when adding to a map
      * @param value, a value attached to a key
      */
@@ -28,26 +29,24 @@ public class MyHashMap <K, V> {
             return;
         }
         int hash = hash(key);
-        
-        Entry<K, V> entry = new Entry<K,V>(key, value, null);
-        
-        if (table[hash]==null) {
+
+        Entry<K, V> entry = new Entry<K, V>(key, value, null);
+
+        if (table[hash] == null) {
             table[hash] = entry;
-        }
-        else {
+        } else {
             Entry<K, V> previous = null;
             Entry<K, V> current = table[hash];
-            
-            while(current!=null) {
-                if(current.key.equals(key)) {
-                    if(previous==null) {
-                        entry.next=current.next;
-                        table[hash]=entry;
+
+            while (current != null) {
+                if (current.key.equals(key)) {
+                    if (previous == null) {
+                        entry.next = current.next;
+                        table[hash] = entry;
                         return;
-                    }
-                    else {
-                        entry.next=current.next;
-                        previous.next=entry;
+                    } else {
+                        entry.next = current.next;
+                        previous.next = entry;
                         return;
                     }
                 }
@@ -56,24 +55,24 @@ public class MyHashMap <K, V> {
             }
             previous.next = entry;
         }
-            
+
     }
-    
+
     /**
      * Method to access a value attached to a certain key
-     * @param key, received as parameter, the key to which the desired value is 
+     *
+     * @param key, received as parameter, the key to which the desired value is
      * attached
      * @return the value attached to the key
      */
     public V get(K key) {
         int hash = hash(key);
-        if(table[hash] == null) {
+        if (table[hash] == null) {
             return null;
-        }
-        else {
+        } else {
             Entry<K, V> temp = table[hash];
-            while(temp!=null) {
-                if(temp.key.equals(key)) {
+            while (temp != null) {
+                if (temp.key.equals(key)) {
                     return temp.value;
                 }
                 temp = temp.next;
@@ -82,37 +81,39 @@ public class MyHashMap <K, V> {
         System.out.println("Avaimella ei löydy arvoa");
         return null;
     }
-    
+
     /**
      * Method to check if MyHashMap contains a certain key
+     *
      * @param key, received as a parameter, the searched key
      * @return the key that was searched
-     */   
+     */
     public K contains(K key) {
         int hash = hash(key);
-        if(table[hash] ==null) {
+        if (table[hash] == null) {
             return null;
         } else {
             Entry<K, V> temp = table[hash];
-            while(temp!=null){
-                if(temp.key.equals(key)) {
+            while (temp != null) {
+                if (temp.key.equals(key)) {
                     return key;
                 }
                 temp = temp.next;
             }
             return null;
         }
-        
+
     }
-    
+
     /**
      * Method to implement hashing function, used to find a location to store
      * key-value-pairs
+     *
      * @param key, a key for which the hash value is calculated
      * @return hash value
      */
     private int hash(K key) {
         return Math.abs(key.hashCode()) % capacity;
     }
-      
+
 }
